eefa9e5356709709c88fbd248413c61023d52d32
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2018-11-09 22:13:22 +0700

    update tampilan side-bar

--- client/app/components/side-bar/side-bar.js
+++ client/app/components/side-bar/side-bar.js
@@ -1,8 +1,7 @@
 angular.module('infomet_nelayan')
     .component('sideBar', {
         bindings: {
-            map: '=',
-            lastLayer: '='
+            map: '='
         },
         controller: ['$scope', 'api', class sideBar {
             constructor($scope, api) {
@@ -11,44 +10,60 @@ angular.module('infomet_nelayan')
             }
 
             $onInit() {
-                this.scope.simpan = () => {
-                    let res = {
-                        type: 'FeatureCollection',
-                        features: []
-                    };
-
-                    this.map.eachLayer(function (layer) {
-                        if (layer['pm'] != undefined) {
-                            layer = layer.toGeoJSON();
-                            if (layer.type === 'Feature') {
-                                res.features.push(layer);
-                            }
-                        }
-                    });
-
-                    if (res.features.length != 0) {
-                        console.log('menuContainer simpan', res);
-                    }
-                };
-
                 this.api.getWilayah()
                     .then((res) => {
                         this.scope.wilayah = res;
                     });
 
-                this.scope.selectWilayah = (wilayah) => {
-                    this.api.getAreaGeoJSON(wilayah)
+                // pengolahan wilayah
+                this.lastWilayah = {};
+                this.scope.eventChangeWilayah = () => {
+                    this.api.getAreaGeoJSON(this.scope.wilayahSelected)
                         .then((res) => {
-                            if (typeof (this.lastLayer.layer) !== 'undefined') {
-                                this.lastLayer.layer.removeFrom(this.map);
+                            // set layer
+                            if ((this.lastWilayah).hasOwnProperty('layer')) {
+                                this.lastWilayah.layer.removeFrom(this.map);
                             }
-                            this.lastLayer['wilayah'] = wilayah;
-                            this.lastLayer['layer'] = L.geoJSON(res);
-                            this.map.fitBounds(this.lastLayer.layer.getBounds());
-                            this.lastLayer.layer.addTo(this.map);
+                            this.lastWilayah['wilayah'] = this.scope.wilayahSelected;
+                            this.lastWilayah['layer'] = L.geoJSON(res);
+                            this.map.fitBounds(this.lastWilayah.layer.getBounds());
+                            this.lastWilayah.layer.addTo(this.map);
 
-                            console.log(res, this.lastLayer.layer.toGeoJSON());
+                            // get area
+                            this.scope.area = res.features;
                         });
+                };
+
+                // pengolahan highlight
+                this.lastHighlightArea = {};
+                this.scope.highlightArea = (i) => {
+                    this.lastHighlightArea = L.geoJSON(this.lastWilayah.layer.toGeoJSON().features[i]).setStyle({ fillColor: 'red' });
+                    this.lastHighlightArea.addTo(this.map);
+                };
+                this.scope.removeHighlightArea = () => {
+                    this.lastHighlightArea.removeFrom(this.map);
+                };
+
+                // pengolahan nama area
+                this.scope.updateAreaName = (i, areaName) => {
+                    this.scope.area[i].properties.name = areaName;
+
+                    // update lastWilayah
+                    this.lastWilayah.layer.removeFrom(this.map);
+                    let tempLayerGeoJSON = this.lastWilayah.layer.toGeoJSON();
+                    tempLayerGeoJSON.features = this.scope.area;
+                    this.lastWilayah.layer = L.geoJSON(tempLayerGeoJSON);
+                    this.lastWilayah.layer.addTo(this.map);
+                };
+
+                // hapus area
+                this.scope.removeArea = (i) => {
+                    this.lastWilayah.layer.removeFrom(this.map);
+                    let tempLayerGeoJSON = this.lastWilayah.layer.toGeoJSON();
+                    tempLayerGeoJSON.features.splice(i, 1);
+                    this.scope.area = tempLayerGeoJSON.features;
+                    this.lastWilayah.layer = L.geoJSON(tempLayerGeoJSON);
+                    this.lastWilayah.layer.addTo(this.map);
                 };
             }
         }],
--- client/app/js/index.js
+++ client/app/js/index.js
@@ -3,5 +3,3 @@ require('./api');
 require('../components/header-container/header-container');
 require('../components/map-container/map-container');
 require('../components/side-bar/side-bar');
-require('../components/modal-tambah-wilayah/modal-tambah-wilayah');
-require('../components/modal-edit-wilayah/modal-edit-wilayah');
