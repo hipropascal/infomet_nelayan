9cbdc650621fc54a6f9f8b965efbe073ee0e98d7
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2018-11-22 18:35:20 +0700

    tambah fungsi get_capture

--- client/app_pi/app.py
+++ client/app_pi/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, jsonify, send_file, request
+from io import BytesIO
+import picamera
 
 app = Flask(__name__, static_url_path="", static_folder="static")
 
@@ -8,6 +10,14 @@ def index():
     return app.send_static_file('index.html')
 
 
+@app.route('/get_capture/')
+def get_capture():
+    temp = BytesIO()
+    with picamera.PiCamera() as camera:
+        camera.capture(temp, format='jpeg')
+        temp.seek(0)
+    return send_file(temp, mimetype='image/jpeg')
+
+
 if __name__ == '__main__':
-    #     inawave_handler()
     app.run(host='0.0.0.0', port=8000, debug=True)
